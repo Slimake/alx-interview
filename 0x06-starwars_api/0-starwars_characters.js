8c35809243e7f1ec4e270c3841209b167ad9fe6a
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-15 00:03:04 +0100

    Modify logic, use Promise.all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,18 +3,34 @@ const request = require('request');
 
 const arg = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${arg}/`;
-request(url, (error, response, body) => {
-  if (error) console.log(error);
-  else if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    data.characters.forEach(url => {
-      request(url, (error, response, body) => {
-        if (error) console.log(error);
-        else if (response.statusCode === 200) {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        }
-      });
+
+// Wrap the request in a Promise
+function requestPromise (options) {
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (error) reject(error);
+      if (response && response.statusCode === 200) {
+        resolve(body);
+      }
     });
+  });
+}
+
+async function fetchData () {
+  try {
+    // Await the request wrapped in a promise
+    const response = await requestPromise({ url, json: true });
+    const promises = response.characters.map(url => {
+      return requestPromise({ url, json: true });
+    });
+
+    const responses = await Promise.all(promises);
+    responses.forEach(response => {
+      console.log(response.name);
+    });
+  } catch (error) {
+    console.log(error);
   }
-});
+}
+
+fetchData();
